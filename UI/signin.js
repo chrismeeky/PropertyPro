6a1fafc534930d665dbb48fc33e87051a466491b
Christian <nwodochristian@gmail.com>
2019-06-14 12:45:18 +0100

    ft(Sign in Page): create sign in user interface where users can login with their email and password [starts #166657831]

---
+++ UI/signin.js
@@ -0,0 +1,28 @@
+const header = document.querySelector('header');
+const menuIcon = document.querySelector('.icon');
+stickyFrom = header.offsetTop;
+
+
+window.onscroll =  () =>{
+     myFunction ();
+}
+
+const myFunction = ()=> {
+if (window.pageYOffset > stickyFrom) {
+    header.classList.add("sticky");
+}
+else {
+    header.classList.remove("sticky");
+}
+}
+
+
+const responsiveNav = () => {
+    let x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+menuIcon.addEventListener('click' ,responsiveNav);

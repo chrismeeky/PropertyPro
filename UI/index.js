c697ec7fa0dda4695e4289d9e52c10aaf3828bf7
Christian <nwodochristian@gmail.com>
2019-06-17 10:48:11 +0100

    feature(users can view all property): Add a responsive sidebar to show featured ads [#finishes #166657929]

--- UI/index.js
+++ UI/index.js
@@ -1,20 +1,24 @@
 const propertyDiv = document.querySelector('.left-sidebar');
+const featuredDiv = document.querySelector('.right-sidebar');
+const featuredHolder = document.createElement('div');
+featuredHolder.setAttribute('class' , 'featured-holder');
+featuredDiv.appendChild(featuredHolder);
 const properties = [
-  {imgSrc:'img1.png', 'id': '1' ,'type':'mini flat' ,'title':'', 'discription':'','price':'N745,875' ,'for':'Sale'},
-  {imgSrc:'image2.png', 'id': '1' ,'type':'mini flat' ,'title':'', 'discription':'','price':'N658,875','for':'Rent'},
-  {imgSrc:'img3.png', 'id': '1' ,'type':'mini flat' ,'title':'', 'discription':'','price':'N1,258,875','for':'Sale'},
-  {imgSrc:'img4.png', 'id': '1' ,'type':'mini flat' ,'title':'', 'discription':'','price':'N698,875','for':'Sale'},
-  {imgSrc:'img5.png', 'id': '1' ,'type':'mini flat' ,'title':'', 'discription':'','price':'N265,875','for':'Rent'},
-  {imgSrc:'img6.png', 'id': '1' ,'type':'mini flat' ,'title':'', 'discription':'','price':'N72,254,875','for':'Sale'},
-  {imgSrc:'img7.png', 'id': '1' ,'type':'mini flat' ,'title':'', 'discription':'','price':'N124,875','for':'Rent'},
-  {imgSrc:'img8.png', 'id': '1' ,'type':'mini flat' ,'title':'', 'discription':'','price':'N250,875','for':'Sale'},
-  {imgSrc:'img9.png', 'id': '1' ,'type':'mini flat' ,'title':'', 'discription':'','price':'N74,875','for':'Rent'},
-  {imgSrc:'img10.png', 'id': '1' ,'type':'mini flat' ,'title':'', 'discription':'','price':'N694,875','for':'Sale'},
-  {imgSrc:'img11.png', 'id': '1' ,'type':'mini flat' ,'title':'', 'discription':'','price':'N81,045,875','for':'Rent'},
-  {imgSrc:'img12.png', 'id': '1' ,'type':'mini flat' ,'title':'', 'discription':'','price':'N987,875','for':'Rent'},
-  {imgSrc:'img13.png', 'id': '1' ,'type':'mini flat' ,'title':'', 'discription':'','price':'N1,685,875','for':'Sale'},
-  {imgSrc:'img14.png', 'id': '1' ,'type':'mini flat' ,'title':'', 'discription':'','price':'N6,254,875','for':'Sale'},
-  {imgSrc:'img15.png', 'id': '1' ,'type':'mini flat' ,'title':'', 'discription':'','price':'N69,588,875','for':'Sale'},
+  {imgSrc:'img1.png', id: '1' ,type:'mini flat' ,title:'', description:'Lets add some dummy description to this property and see how long it can hold',price:'N745,875' ,for:'Sale' ,featured:'yes'},
+  {imgSrc:'image2.png', id: '1' ,type:'mini flat' ,title:'', description:'Lets add some dummy description to this property and see how long it can hold',price:'N658,875',for:'Rent',featured:'no'},
+  {imgSrc:'img3.png', id: '1' ,type:'mini flat' ,title:'', description:'Lets add some dummy description to this property and see how long it can hold',price:'N1,258,875',for:'Sale',featured:'no'},
+  {imgSrc:'img4.png', id: '1' ,type:'mini flat' ,title:'', description:'Lets add some dummy description to this property and see how long it can hold',price:'N698,875',for:'Sale',featured:'yes'},
+  {imgSrc:'img5.png', id: '1' ,type:'mini flat' ,title:'', description:'Lets add some dummy description to this property and see how long it can hold',price:'N265,875',for:'Rent',featured:'no'},
+  {imgSrc:'img6.png', id: '1' ,type:'mini flat' ,title:'', description:'Lets add some dummy description to this property and see how long it can hold',price:'N72,254,875',for:'Sale',featured:'no'},
+  {imgSrc:'img7.png', id: '1' ,type:'mini flat' ,title:'', description:'Lets add some dummy description to this property and see how long it can hold',price:'N124,875',for:'Rent',featured:'no'},
+  {imgSrc:'img8.png', id: '1' ,type:'mini flat' ,title:'', description:'Lets add some dummy description to this property and see how long it can hold',price:'N250,875',for:'Sale',featured:'no'},
+  {imgSrc:'img9.png', id: '1' ,type:'mini flat' ,title:'', description:'Lets add some dummy description to this property and see how long it can hold',price:'N74,875',for:'Rent',featured:'no'},
+  {imgSrc:'img10.png', id: '1' ,type:'mini flat' ,title:'', description:'Lets add some dummy description to this property and see how long it can hold',price:'N694,875',for:'Sale',featured:'no'},
+  {imgSrc:'img11.png', id: '1' ,type:'mini flat' ,title:'', description:'Lets add some dummy description to this property and see how long it can hold',price:'N81,045,875',for:'Rent',featured:'no'},
+  {imgSrc:'img12.png', id: '1' ,type:'mini flat' ,title:'', description:'Lets add some dummy description to this property and see how long it can hold',price:'N987,875',for:'Rent',featured:'no'},
+  {imgSrc:'img13.png', id: '1' ,type:'mini flat' ,title:'', description:'Lets add some dummy description to this property and see how long it can hold',price:'N1,685,875',for:'Sale',featured:'yes'},
+  {imgSrc:'img14.png', id: '1' ,type:'mini flat' ,title:'', description:'Lets add some dummy description to this property and see how long it can hold',price:'N6,254,875',for:'Sale',featured:'no'},
+  {imgSrc:'img15.png', id: '1' ,type:'mini flat' ,title:'', description:'Lets add some dummy description to this property and see how long it can hold',price:'N69,588,875',for:'Sale',featured:'no'},
   
 ]
 
@@ -25,7 +29,7 @@ properties.forEach(element =>{
   price.textContent = `${element.price} (for ${element.for})`;
   price.setAttribute('class', 'price');
   let description = document.createElement('p');
-  description.textContent = 'this is the description';
+  description.textContent = element.description;
   description.setAttribute('class', 'description');
   let imgDiv = document.createElement('div');
   let innerImgdiv = document.createElement('div');
@@ -33,12 +37,21 @@ properties.forEach(element =>{
   image.setAttribute('src', element.imgSrc);
   innerImgdiv.setAttribute('class', 'inner-image-div');
   innerImgdiv.appendChild(image);
+  let detailsDiv = document.createElement('div');
+  detailsDiv.setAttribute('class' , 'details-div');
+  detailsDiv.appendChild(price);
+  detailsDiv.appendChild(description);
   
   imgDiv.setAttribute('class' ,'image-div');
   imgDiv.appendChild(innerImgdiv ,price);
   imgDiv.appendChild(price);
-  imgDiv.appendChild(description);
+  imgDiv.appendChild(detailsDiv);
 propertyDiv.appendChild(imgDiv);
+if(element.featured == 'yes'){
+  description.parentNode.removeChild(description);
+  featuredHolder.appendChild(imgDiv);
+  imgDiv.setAttribute('class' ,'image-div-rightbar');
+}
 })
 
 

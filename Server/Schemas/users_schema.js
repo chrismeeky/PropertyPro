4a0b7c43c311085959c5c79a51261e7ab8949d2f
Christian <nwodochristian@gmail.com>
2019-07-09 10:00:07 +0100

    chore(sign up endpoint): Create sign up endpoint using psql [finishes #167160009]

--- Server/Schemas/users_schema.js
+++ Server/Schemas/users_schema.js
@@ -5,7 +5,8 @@ const schema = Joi.object().keys({
 	last_name: Joi.string().trim().max(20).required(),
 	password: Joi.string().trim().min(8).max(16).required(),
 	phoneNumber: Joi.string().trim().max(15).min(8).required(),
+	state: Joi.string().trim().max(20).min(3).required(),
+	city: Joi.string().trim().max(20).min(3).required(),
 	address: Joi.string().trim().min(10).max(50).required(),
-	is_admin: Joi.boolean().required(),
 });
 export default schema;

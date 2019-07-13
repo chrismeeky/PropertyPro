a44b8f87f74b57e10820001c26f6dbfd5baa36f0
Christian <nwodochristian@gmail.com>
2019-07-13 11:13:59 +0100

    chore(set up database): Set cloud database for postgresql [finishes #167270848]

--- Server/config/pool.js
+++ Server/config/pool.js
@@ -1,7 +1,7 @@
 import { Pool } from 'pg';
 require('dotenv').config()
 const pool = new Pool({
-    connectionString: process.env.DB_URI,
+    connectionString: process.env.DB_LOCAL,
   })
 
   export default pool;
